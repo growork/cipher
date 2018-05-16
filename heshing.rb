86702c43d9bba31ae7ce99d6490580c333299e86
growork <growork@mail.ru>
2018-05-16 23:33:48 +0300

    Added sha2

--- heshing.rb
+++ heshing.rb
@@ -1,17 +1,25 @@
 require 'digest'
 
-puts "Введите сообщение:"
+puts 'Введите сообщение:'
 
 message = gets.chomp
 
-puts "Выберите способ шифрования:"
-puts "1. MD5"
-puts "2. SHA1"
+puts 'Выберите способ шифрования:'
+puts '1. MD5'
+puts '2. SHA1'
+puts '3. SHA2'
 
-choise = gets.chomp
+choise = gets.to_i
 
 a = ""
 
-choise == "1" ? (a = Digest::MD5.hexdigest message):(a = Digest::SHA1.hexdigest message)
-
-puts "Вот что получилось: #{a}"
+case choise
+when 1
+  puts "Вот что получилось: #{Digest::MD5.hexdigest message}"
+when 2
+  puts "Вот что получилось: #{Digest::SHA1.hexdigest message}"
+when 3
+  puts "Вот что получилось: #{Digest::SHA2.hexdigest message}"
+else
+  puts 'Введены неверные данные!'
+end
